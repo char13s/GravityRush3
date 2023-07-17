19f81869051b9206694570909bacfa74d0909193
Hitoshi Kanno <hkannok@gmail.com>
2023-07-17 14:36:16 -0500

    First push of Unreal project

---
+++ Source/ProtoGravityShift/ProtoGravityShiftGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProtoGravityShiftGameMode.h"
+#include "ProtoGravityShiftCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AProtoGravityShiftGameMode::AProtoGravityShiftGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/ProtoGravityShift/ProtoGravityShiftGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProtoGravityShiftGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProtoGravityShiftGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProtoGravityShiftGameMode();
+};
+
+
+
